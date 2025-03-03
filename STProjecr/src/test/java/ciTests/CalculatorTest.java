0ec880b7dde1c3b7d067657c1c2a52934583c224
Hamza Ashraf <hamzaashraf121@gmail.com>
2025-03-03 14:09:16 +0100

    Added TestNG suite and Surefire plugin

--- STProjecr/src/test/java/ciTests/CalculatorTest.java
+++ STProjecr/src/test/java/ciTests/CalculatorTest.java
@@ -1,4 +1,19 @@
 package ciTests;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class CalculatorTest {
+
+    @Test
+    public void testAddition() {
+        Calculator calc = new Calculator();
+        Assert.assertEquals(calc.add(2, 3), 5);
+    }
+
+    @Test
+    public void testSubtraction() {
+        Calculator calc = new Calculator();
+        Assert.assertEquals(calc.subtract(5, 3), 2);
+    }
 }
