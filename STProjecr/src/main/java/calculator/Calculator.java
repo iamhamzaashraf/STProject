8cf6d8c9080bd4a28f2168b8edf12df08e9a1ed9
Hamza Ashraf <hamzaashraf121@gmail.com>
2025-03-03 14:42:47 +0100

    Empty Calculator Class

--- STProjecr/src/main/java/calculator/Calculator.java
+++ STProjecr/src/main/java/calculator/Calculator.java
@@ -1,11 +1,5 @@
 package calculator;
 
 public class Calculator {
-    public int add(int a, int b) {
-        return a + b;
-    }
 
-    public int subtract(int a, int b) {
-        return a - b;
-    }
 }
