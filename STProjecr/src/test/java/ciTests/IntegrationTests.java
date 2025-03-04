69ab95dda0e0cfb42b8f4ade7d48180fb7aa545d
Hamza Ashraf <hamzaashraf121@gmail.com>
2025-03-04 13:56:35 +0100

    Added Integration Tests

---
+++ STProjecr/src/test/java/ciTests/IntegrationTests.java
@@ -0,0 +1,37 @@
+package ciTests;
+
+import calculator.Calculator;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class IntegrationTests {
+
+    private Calculator calc;
+
+    @BeforeMethod
+    public void setUp() {
+        calc = new Calculator();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        calc = null;
+    }
+
+    @Test
+    public void testCombinedOperations() {
+        int sum = calc.add(10, 5);
+        int result = calc.subtract(sum, 3);
+        Assert.assertEquals(result, 12);
+    }
+
+    @Test
+    public void testSequentialOperations() {
+        int result = calc.add(2, 2);
+        result = calc.add(result, 6);
+        result = calc.subtract(result, 3);
+        Assert.assertEquals(result, 7);
+    }
+}
