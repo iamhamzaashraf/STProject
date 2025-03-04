8ec52c2ed34e8ba4c60ac68f3f9dd8892e2ee22b
Hamza Ashraf <hamzaashraf121@gmail.com>
2025-03-04 13:34:45 +0100

    Subtract Method is added let it fail

--- STProjecr/src/test/java/ciTests/CalculatorTest.java
+++ STProjecr/src/test/java/ciTests/CalculatorTest.java
@@ -1,68 +1,32 @@
-/*package ciTests;
-
-import calculator.Calculator;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-public class CalculatorTest {
-
-    @Test
-    public void testAddition() { // Addition Test
-        Calculator calc = new Calculator();
-        int result = calc.add(2, 3); // Test case for "add" Method
-        Assert.assertEquals(result, 5);
-    }
-
-    @Test
-    public void testSubtraction() {
-        Calculator calc = new Calculator();
-        Assert.assertEquals(calc.subtract(5, 3), 2);
-
-    }
-}*/
-
 package ciTests;
 
-import org.testng.Assert;
-import org.testng.annotations.*;
 import calculator.Calculator;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class CalculatorTest {
 
     private Calculator calc;
 
-    @BeforeClass
-    public void setupClass() {
-        System.out.println("Starting CalculatorTest suite...");
-    }
-
     @BeforeMethod
-    public void setupMethod() {
+    public void setUp() {
         calc = new Calculator();
     }
 
     @AfterMethod
-    public void teardownMethod() {
-        System.out.println("Test completed.");
+    public void tearDown() {
+        calc = null;
     }
 
-    @Test(dataProvider = "additionData")
-    public void testAddition(int a, int b, int expected) {
-        int result = calc.add(a, b);
-        verifyResult(result, expected);
+    @Test
+    public void testAddition() {
+        Assert.assertEquals(calc.add(2, 3), 5);
     }
 
-    @DataProvider(name = "additionData")
-    public Object[][] provideAdditionData() {
-        return new Object[][]{
-                {2, 3, 5},
-                {10, 5, 15},
-                {-1, 1, 0},
-                {0, 0, 0}
-        };
-    }
-
-    private void verifyResult(int actual, int expected) {
-        Assert.assertEquals(actual, expected, "Result mismatch!");
+    @Test
+    public void testSubtraction() {
+        Assert.assertEquals(calc.subtract(5, 3), 2);
     }
 }
