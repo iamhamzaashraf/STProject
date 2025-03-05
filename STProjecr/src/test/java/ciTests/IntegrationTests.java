b70cf58df7230c9e75c05c0ddc1e9096f19708b1
Hamza Ashraf <hamzaashraf121@gmail.com>
2025-03-05 14:24:57 +0100

    Resolved Integration Bug

--- STProjecr/src/test/java/ciTests/IntegrationTests.java
+++ STProjecr/src/test/java/ciTests/IntegrationTests.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
 public class IntegrationTests {
 
     private Calculator calc;
-Wha
+
     @BeforeMethod
     public void setUp() {
         calc = new Calculator();
