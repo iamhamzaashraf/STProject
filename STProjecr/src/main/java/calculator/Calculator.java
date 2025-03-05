59659f69d0b5fca63ab5b02ac251ec79d749de23
Ahmed Ali <ahmedali.feb1997@gmail.com>
2025-03-05 14:22:17 +0100

    Change value Names

--- STProjecr/src/main/java/calculator/Calculator.java
+++ STProjecr/src/main/java/calculator/Calculator.java
@@ -1,8 +1,8 @@
 package calculator;
 
 public class Calculator {
-    public int add(int a, int b) {
-        return a + b;
+    public int add(int val1, int val2) {
+        return val1 + val2;
     }
     public int subtract(int a, int b) {
         return a - b;
