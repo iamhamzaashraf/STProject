1dbbb497e7f25c2e0018393c4f8178ab0a101a35
Hamza Ashraf <hamzaashraf121@gmail.com>
2025-03-08 15:17:44 +0100

    Removed extra dependencies

--- STProjecr/src/test/java/ciTests/SystemTest.java
+++ STProjecr/src/test/java/ciTests/SystemTest.java
@@ -1,9 +1,6 @@
 package ciTests;
 
-import calculator.Calculator;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class SystemTest extends BaseTest{
