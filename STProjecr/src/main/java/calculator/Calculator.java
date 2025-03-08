f919c35a672cb1197f302aeae285e6ea524af622
Ahmed Ali <ahmedali.feb1997@gmail.com>
2025-03-08 14:48:15 +0100

    Changed Division class

--- STProjecr/src/main/java/calculator/Calculator.java
+++ STProjecr/src/main/java/calculator/Calculator.java
@@ -12,7 +12,7 @@ public class Calculator {
     }
     public int divide(int a, int b) {
         if (b == 0) {
-            throw new IllegalArgumentException("Division by zero is not allowed.");
+            throw new ArithmeticException("Division by zero is not allowed.");
         }
         return a / b;
 }
