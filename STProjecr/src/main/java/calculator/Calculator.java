7734432b69e0363523ed230d473020040f53df93
Hamza Ashraf <hamzaashraf121@gmail.com>
2025-03-08 15:15:49 +0100

    Fixed back

--- STProjecr/src/main/java/calculator/Calculator.java
+++ STProjecr/src/main/java/calculator/Calculator.java
@@ -2,6 +2,9 @@ package calculator;
 
 public class Calculator {
 
+    public int add(int val1, int val2) {
+        return val1 + val2;
+    }
     public int subtract(int a, int b) {
         return a - b;
     }
