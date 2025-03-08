034882d509569204aa24169ea4c939cca3962d4e
Hamza Ashraf <hamzaashraf121@gmail.com>
2025-03-08 15:02:07 +0100

    Added Integration Test part of Test Suit

--- STProjecr/src/test/java/ciTests/SystemTest.java
+++ STProjecr/src/test/java/ciTests/SystemTest.java
@@ -6,19 +6,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class SystemTest {
-
-    private Calculator calc;
-
-    @BeforeMethod
-    public void setUp() {
-        calc = new Calculator();
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        calc = null;
-    }
+public class SystemTest extends BaseTest{
 
     @Test
     public void testFullWorkflow() {
