112dda8efd2860bbd271f40a597c00d8de139a99
Hamza Ashraf <hamzaashraf121@gmail.com>
2025-03-08 13:42:47 +0100

    Updated Modularization approach for Unit Test

--- STProjecr/src/test/java/ciTests/CalculatorTest.java
+++ STProjecr/src/test/java/ciTests/CalculatorTest.java
@@ -6,51 +6,21 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class CalculatorTest {
+public class CalculatorTest extends BaseTest {
 
-    private Calculator calc;
-
-    @BeforeMethod
-    public void setUp() {
-        calc = new Calculator();
-    }
-
-    // Set Values to null after completion
-    @AfterMethod
-    public void tearDown() {
-        calc = null;
-    }
-
-    // Unit Test for Addition
     @Test
     public void testAddition() {
-        Assert.assertEquals(calc.add(2, 3), 5);
-    }
-    // Unit Test for Subtraction
-    @Test
-    public void testSubtraction() {
-        Assert.assertEquals(calc.subtract(5, 3), 2);
-    }
-    // Test Multiplication
-    @Test
-    public void testMultiply() {
-        Assert.assertEquals(calc.multiply(4, 3), 12);
-    }
-    // Test Division
-    @Test
-    public void testDivision() {
-        Assert.assertEquals(calc.divide(10, 2), 5);
+        Assert.assertEquals(calc.add(2, 3), 5); // Test simple addition
     }
 
     @Test
-    // Unit Test to test addition with zero
-    public void testAdditionWithZero() {
-        Assert.assertEquals(calc.add(0, 5), 5);
+    public void testSubtraction() {
+        Assert.assertEquals(calc.subtract(5, 3), 2); // Test simple subtraction
     }
+
     @Test
-    // Unit Test for subtration with negative
-    public void testSubtractionWithNegative() {
-        Assert.assertEquals(calc.subtract(3, -2), 5);
+    public void testDivision() {
+        Assert.assertEquals(calc.divide(10, 2), 5); // Test simple division
     }
 
 }
