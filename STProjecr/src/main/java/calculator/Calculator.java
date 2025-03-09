5428249120158fab705ed97d9ab47e938f04487f
tazeemalik <tazeemalik@gmail.com>
2025-03-09 15:43:30 +0100

    Removing the Multiplication function for testing

--- STProjecr/src/main/java/calculator/Calculator.java
+++ STProjecr/src/main/java/calculator/Calculator.java
@@ -9,9 +9,7 @@ public class Calculator {
     public int subtract(int a, int b) {
         return a - b;
     }
-    public int multiply(int a, int b) {
-        return a * b;
-    }
+
     public int divide(int a, int b) {
         if (b == 0) {
             throw new ArithmeticException("Division by zero is not allowed.");
