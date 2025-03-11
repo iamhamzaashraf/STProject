d9f2b93a5852bfb340d039af0422fe7d874943f1
Bhaskar Nagireddy <bhaskarnagireddy888@gmail.com>
2025-03-11 02:05:36 +0100

    Deleted the dividend function

--- STProjecr/src/main/java/calculator/Calculator.java
+++ STProjecr/src/main/java/calculator/Calculator.java
@@ -11,11 +11,6 @@ public class Calculator {
     }
     public int multiply(int a, int b) {
         return a * b;
-    }
-    public int divide(int a, int b) {
-        if (b == 0) {
-            throw new ArithmeticException("Division by zero is not allowed.");
-        }
-        return a / b;
+
 }
 }
