3dd1cee1a095681679a170a11f6b519e8de349d3
Ahmed Ali <ahmedali.feb1997@gmail.com>
2025-03-12 21:47:39 +0100

    Deleting the code for test

--- STProjecr/src/main/java/calculator/Calculator.java
+++ STProjecr/src/main/java/calculator/Calculator.java
@@ -2,10 +2,7 @@ package calculator;
 
 public class Calculator {
 
-    public int add(int val1, int val2) {
 
-        return val1 + val2;
-    }
     public int subtract(int a, int b) {
         return a - b;
     }
